f0d806c089a93a1a48c9ae88167a894b6acc0489
Santi <santima1904@gmail.com>
2021-10-27 14:36:23 +0200

    pruebas hilos

--- PracticaClaseHayQueCorregir/src/PruebaContarArray/Contenedor2.java
+++ PracticaClaseHayQueCorregir/src/PruebaContarArray/Contenedor2.java
@@ -1,9 +1,10 @@
 package PruebaContarArray;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Contenedor2<T> {
-    private List<int[]> datos;
+    private List<int[]> datos = new ArrayList<>();
 
     synchronized public int[] get() {
         int [] result = datos.get(0);
@@ -12,11 +13,11 @@ public class Contenedor2<T> {
     }
 
     synchronized public void put(int[] valor) {
-        this.datos.add(valor);
+        datos.add(valor);
     }
 
     synchronized public  boolean maximoAlcanzado() {
-        return (this.datos.size() == 10);
+        return (datos.size() == 10);
     }
 
     synchronized public boolean vacio() {
--- PracticaClaseHayQueCorregir/src/PruebaContarArray/Productor.java
+++ PracticaClaseHayQueCorregir/src/PruebaContarArray/Productor.java
@@ -6,34 +6,40 @@ import java.security.SecureRandom;
 public class Productor  implements Runnable {
 
     private final Contenedor2 datos;
+    private  String nombre;
 
-    public Productor(Contenedor2 datos) {
+    public Productor(Contenedor2 datos, String nombre) {
         this.datos = datos;
+        this.nombre = nombre;
+    }
+
+    private int[] producirDato() {
+        int[] numeros = new int[100];
+        for (int i = 0; i < 100; i++) {
+            SecureRandom random = new SecureRandom();
+            numeros[i] = random.nextInt();
+        }
+        return numeros;
     }
 
 
     @Override
     public void run() {
+        int [] dato;
         while (true) {
+
+            dato = producirDato();
             synchronized (this.datos) {
-                while (!this.datos.maximoAlcanzado()) {
+                while (this.datos.maximoAlcanzado()) {
                     try {
                         this.datos.wait();
                     } catch (InterruptedException ex) {
                     }
                 }
-                this.datos.put(producirDato());
+                this.datos.put(dato);
                 this.datos.notifyAll();
             }
         }
     }
+}
 
-    private int[] producirDato() {
-            int[] numeros = new int[100];
-            for (int i = 0; i < 100; i++) {
-                SecureRandom random = new SecureRandom();
-                numeros[i] = random.nextInt();
-            }
-            return numeros;
-        }
-    }
